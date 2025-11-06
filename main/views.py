b7df122140cc3fc3a703a7efd68c69238c01daea
Ziyo <uzshelby532@gmail.com>
2025-11-07 00:38:17 +0500

    Fix duplicate functions in views.py

--- main/views.py
+++ main/views.py
@@ -2,20 +2,3 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'main/home.html')
-
-def test(request):
-    return render(request, 'main/test.html')
-def webapp_home(request):
-    return render(request, 'main/webapp_home.html')
-
-def webapp_test(request):
-    return render(request, 'main/webapp_test.html')
-
-def webapp_practice(request):
-    return render(request, 'main/webapp_practice.html')
-
-def webapp_topics(request):
-    return render(request, 'main/webapp_topics.html')
-
-def webapp_results(request):
-    return render(request, 'main/webapp_results.html')
